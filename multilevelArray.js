70642fbf3988e091ea933fc544f4cadb542fd157
Jyoti152317 <engjyoti18@gmail.com>
2024-12-08 19:35:48 +0530

    third commit

---
+++ multilevelArray.js
@@ -0,0 +1,17 @@
+const people = [
+  {
+    name: "Alice",
+    address: { city: "New York", street: { name: "Broadway", number: 123 } },
+  },
+  {
+    name: "Bob",
+    address: {
+      city: "Los Angeles",
+      street: { name: "Sunset Boulevard", number: 456 },
+    },
+  },
+];
+const arr = people.map((ele) => {
+  return `${ele.name} lives in ${ele.address.city} on ${ele.address.street.name}`;
+});
+console.log(arr);
